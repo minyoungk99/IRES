32d9c6dd05dbda597ae205731b92eb9a31073159
Min Young <MYK@KimMinui-MacBook-Pro.local>
2019-08-14 09:30:49 -0700

    working on sigtest setup

--- R_wavelet/wavelet_sigtest.R
+++ R_wavelet/wavelet_sigtest.R
@@ -1,8 +1,11 @@
 #Perform wavelet significance test on profile ratios
 setwd("/Users/MYK/Desktop/IRES/R_wavelet")
 library('biwavelet')
+source('adv_biwavelet_packages.R')
 
 x <- matrix(1:714, nrow=1)
+mjd <- read.table('../J0332_MJD.txt')
+mjd <- mjd[-1,1][1] #drop first MJD
 ratios <- read.table('J0332_ratios.txt')
 #combine these two into 2x714 matrix required format for wt.sig
 
