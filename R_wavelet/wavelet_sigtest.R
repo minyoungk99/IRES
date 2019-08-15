1049a5884ecb7a1c1e21aa328cb3f58b4ba8e481
Min Young <MYK@KimMinui-MacBook-Pro.local>
2019-08-15 03:36:39 -0700

    included importing fields package required for colorbar in plot

--- R_wavelet/wavelet_sigtest.R
+++ R_wavelet/wavelet_sigtest.R
@@ -1,7 +1,7 @@
 #Perform wavelet significance test on profile ratios
 setwd("/Users/MYK/Desktop/IRES/R_wavelet")
-library('biwavelet')
-library('pracma')
+packages <- c('biwavelet', 'pracma', 'fields')
+sapply(packages, FUN=library, character.only=TRUE)
 source('adv_biwavelet_packages.R')
 
 mjd <- read.table('../J0332_MJD.txt')
@@ -10,5 +10,8 @@ ratios <- read.table('J0332_ratios.txt')
 #combine these two into 2x714 matrix required format for wt.sig
 #convert each observation of a variable to numeric by sapply(ratios[1,], MARGIN=1,FUN=as.numeric)
 
-
+i <- 3
+wt_obj <- wt_area(cbind(1:714, sapply(ratios[i,], MARGIN=1, FUN=as.numeric)), 
+                        lag1=.00000001, Nnull=10000, anrands = 1000)
+plot.biwavelet_adv(wt_obj)
 
