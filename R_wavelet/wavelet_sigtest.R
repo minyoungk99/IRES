19e0637f16742578b49574694fe6e5a34516d44b
Min Young <MYK@KimMinui-MacBook-Pro.local>
2019-08-15 05:21:12 -0700

    Finished producing plots for ratios

--- R_wavelet/wavelet_sigtest.R
+++ R_wavelet/wavelet_sigtest.R
@@ -5,13 +5,25 @@ lapply(packages, FUN=library, character.only=TRUE)
 source('adv_biwavelet_packages.R')
 
 mjd <- read.table('../J0332_MJD.txt')
-mjd <- mjd[-1,1][1] #drop first MJD
+mjd <- mjd[-1,1] #drop first MJD
+profs <- read.table('../J0332_profs.txt')
 ratios <- read.table('J0332_ratios.txt')
-#combine these two into 2x714 matrix required format for wt.sig
+#combine these two into 714x2 matrix required format for wt.sig
 #convert each observation of a variable to numeric by sapply(ratios[1,], MARGIN=1,FUN=as.numeric)
 
-i <- 3
-wt_obj <- wt_area(cbind(1:714, sapply(ratios[i,], MARGIN=1, FUN=as.numeric)), 
-                        lag1=.00000001, Nnull=10000, anrands = 1000)
-plot.biwavelet_adv(wt_obj)
+nprofs <- dim(ratios)[1]
+nbins <- dim(ratios)[2]
+pw_sig <- .99
+cum_sig <- .99
+
+pdf("power_spectra.pdf", width=5, height=3) 
+for (i in 1:nprofs) {
+  par(oma = c(0, 0, 0, 1), mar = c(5, 4, 4, 5) + 0.1) #make space for colorbar
+  data <- cbind(1:nbins, sapply(ratios[i,], FUN=as.numeric))
+  wt <- wt_area(data, lag1=.0000000000001, sig.level = pw_sig, csig.level=cum_sig)
+  plot.biwavelet_adv(wt, plot.cb=TRUE, main=bquote("MJD:" ~ .(mjd[i])*","~
+                                                     alpha[pw]*":"~.(pw_sig)*","~
+                                                     alpha[cum]*":"~.(cum_sig)))
+}
+dev.off()
 
