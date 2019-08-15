7dfd7fe0a618a78bd175ce6d241d9bcc83167302
Min Young <MYK@KimMinui-MacBook-Pro.local>
2019-08-15 03:21:48 -0700

    still writing sig test. Also confirmed you need to calibrate profile before normalizing

--- R_wavelet/wavelet_sigtest.R
+++ R_wavelet/wavelet_sigtest.R
@@ -1,13 +1,14 @@
 #Perform wavelet significance test on profile ratios
 setwd("/Users/MYK/Desktop/IRES/R_wavelet")
 library('biwavelet')
+library('pracma')
 source('adv_biwavelet_packages.R')
 
-x <- matrix(1:714, nrow=1)
 mjd <- read.table('../J0332_MJD.txt')
 mjd <- mjd[-1,1][1] #drop first MJD
 ratios <- read.table('J0332_ratios.txt')
 #combine these two into 2x714 matrix required format for wt.sig
+#convert each observation of a variable to numeric by sapply(ratios[1,], MARGIN=1,FUN=as.numeric)
 
 
 
