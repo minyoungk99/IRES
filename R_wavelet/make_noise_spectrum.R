1ca04ff66ab16ee449e36be9be76b4ec6ad5673d
Min Young <MYK@KimMinui-MacBook-Pro.local>
2019-08-17 10:41:58 -0700

    made some arcwise plots using R

--- R_wavelet/make_noise_spectrum.R
+++ R_wavelet/make_noise_spectrum.R
@@ -3,7 +3,6 @@ setwd("/Users/MYK/Desktop/IRES/R_wavelet")
 packages <- c('biwavelet', 'pracma', 'fields', 'RColorBrewer') #'fields' required for plotting colorbar
 lapply(packages, FUN=library, character.only=TRUE)
 
-data <- rnorm(1000)
-wt_obj <- wt(cbind(1:1000,data), lag1=.00000001)
-dev.off()
+data <- rnorm(2000, sd=.5) + rep(c(rep(0, 99),10), 20)
+wt_obj <- wt(cbind(1:2000,data), lag1=.00000001)
 plot.biwavelet(wt_obj)
