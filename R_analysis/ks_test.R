beb26e9fcaaf85727b4eafffef41ba22854d1e0a
Min Young <MYK@KimMinui-MacBook-Pro.local>
2019-08-21 09:20:37 -0700

    tried picking mode change profile as reference profile for ks test. then no profiles are significant. also redid ks test, due to natural fluctuation in noise when simulating, critical D changes, and many repeats give only 2 significant. so modified report accordingly.

--- R_analysis/ks_test.R
+++ R_analysis/ks_test.R
@@ -2,7 +2,7 @@
 setwd("/Users/MYK/Desktop/IRES/R_analysis")
 
 null <- read.table('null.txt')
-data <- read.table('cdf.txt')
+data <- read.table('sim_cdf.txt')
 D_value <- c()
 
 for (i in seq( dim(data)[1] )) {
@@ -10,5 +10,5 @@ for (i in seq( dim(data)[1] )) {
   D_value[i] <- result$statistic
 }
 
-write.table(D_value, file = "real_Dvalues.txt", row.names = FALSE,
+write.table(D_value, file = "sim_Dvalues.txt", row.names = FALSE,
             col.names = FALSE)
