258cf5769f1494b23cb785a629184237f77292cf
Min Young <MYK@KimMinui-MacBook-Pro.local>
2019-08-28 03:33:49 -0700

    redid test comp, wavelet sig avelet sig tests. need better way to create DataFrame when plotting testcomp, maybe use indices instead of MJD values?

--- R_wavelet/wavelet_sigtest_diffs.R
+++ R_wavelet/wavelet_sigtest_diffs.R
@@ -13,11 +13,11 @@ diffs <- read.table('J0332_diffs.txt')
 
 #EDIT FUNCTION GENERATE_REDNOISE IN adv_biwavelet_packages.R to use DIFFERENCE of WN 
 
-nprofs <- 1#dim(diffs)[1]
+nprofs <- dim(diffs)[1]
 nbins <- dim(diffs)[2]
 cum_sig <- .99
 
-#pdf("../plots/diffs_99_sd.pdf", width=5, height=3) 
+pdf("../plots/diffs_99.pdf", width=5, height=3) 
 cmap = cm.colors(64)
 for (i in 1:nprofs) {
   par(oma = c(0, 0, 0, 1), mar = c(5, 4, 4, 5) + 0.1) #make space for colorbar
@@ -27,5 +27,5 @@ for (i in 1:nprofs) {
   plot.biwavelet_adv(wt,  plot.cb=TRUE, ncol=64, fill.cols=cmap, lwd.sig=1,
                      main=bquote("MJD:" ~ .(mjd[i])*","~ alpha[cum]*":"~.(1-cum_sig)))
 }
-#dev.off()
+dev.off()
 
